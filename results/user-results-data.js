974eb68e432070af8462e05ad3f454c6da87a480
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-28 09:40:40 -0800

    add user result data

---
+++ results/user-results-data.js
@@ -0,0 +1,18 @@
+export const userEvil = {
+    good: 'You had a fruitful and upstanding career,',
+    gray: 'What you have done in your time as a mediator will affect history for the worse,',
+    evil: 'You are a threat to humanity,'
+};
+
+export const userAnguish = {
+    sane: 'and have come to peace with the decisions you made.',
+    troubled: 'your choices have knocked a few screws loose, and your trauma haunts you.',
+    insane: 'in spite of everything, you can no longer cope with reality and wish for death.'
+};
+
+export const userLives = {
+    few: 'You did your best to maintain a high moral upstanding and directed your client accordingly, saving lives.',
+    many: 'You could have saved more lives if you tried.',
+    extreme: 'You wanted your client\'s people to die.'
+};
+
