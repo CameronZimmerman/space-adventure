6a43dd4adcd343bcd1310100ef9c231196d6d95b
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-27 23:02:49 -0800

    add dispute page functionality

--- dispute-data.js
+++ dispute-data.js
@@ -1,13 +1,16 @@
 const disputeData = [
     {
         id: 'kidnapping',
+        title: 'The kidnapping',
         description: 'That son of a biscuit Gorkoo Natissi has kidnapped my daughter and is holding her randsom for 26,000,000 oblks. If this isn\'t resolved soon I think I may do something rash... what would you advise?',
         leader: 'leader1.png',
+        leaderName: 'Bobjos',
+        bgColor: 'green',
         top: 25,
         left: 62,
         options: [
             {
-                optionId: 'pay',
+                id: 'pay',
                 text: 'you should pay the ransom... your daughter is what is most important',
                 result: 'your client payed the ransom and got his daughter back. This show of weakness leads to a coup causing thousands of lives',
                 extinguishedLives: 10000,
@@ -16,7 +19,7 @@ const disputeData = [
 
             },
             {
-                optionId: 'take',
+                id: 'take',
                 text: 'What Gorkoo and his men have done is wrong, and you can\'t let it stand, send in the troops to take her back',
                 result: 'your client sent in his men to take his daughter back by force. Planetary civil war ensues, the nukes are dropped and everyone is dead',
                 extinguishedLives: 8000000000,
@@ -24,7 +27,7 @@ const disputeData = [
                 anguish: 5         
             },
             {
-                optionId: 'negotiate',
+                id: 'negotiate',
                 text: 'I don\'t think you should give in to Gorkoo\'s terms. See if you can call a meeting with him to "discuss things"',
                 result: 'your client has called a meeting with Gorkoo, but brings all of his troops. His willingness to discuss and show of force intice Gorkoo to return your client\'s daughter in exchange for a lesser reward',
                 extinguishedLives: 0,
@@ -35,13 +38,16 @@ const disputeData = [
     },
     {
         id: 'war',
+        title: 'The Planetary Civil War',
         description: 'I have decided it is time to end the century long civil war that has been raging on my planet, but I need a good way to end it all for good...what would you advise?',
         leader: 'leader2.png',
+        leaderName: 'Olklinn',
+        bgColor: 'blue',
         top: 53,
         left: 22,
         options: [
             {
-                optionId: 'force',
+                id: 'force',
                 text: 'You should end it all with one final barage of nuclear bombs',
                 result: 'as you advise, your client nukes the other side to smitherines. The war is ended, but at what cost?',
                 extinguishedLives: 60000000,
@@ -50,7 +56,7 @@ const disputeData = [
 
             },
             {
-                optionId: 'treaty',
+                id: 'treaty',
                 text: 'You should call a meeting to discuss a peace treaty',
                 result: 'your client calls a peace treaty, and when both leaders meet things go sour. Both call in the nukes and destroy the entire planet all at once.',
                 extinguishedLives: 8000000000,
@@ -58,7 +64,7 @@ const disputeData = [
                 anguish: 5         
             },
             {
-                optionId: 'poison',
+                id: 'poison',
                 text: 'You should send them a poisoned pie... no one can resist a delicious pastry',
                 result: 'your client sends the enemy leader a poisoned boysenberry pie. little did you know the enemy leader is immune and misjudges this murderous act as an act of kindness, ending the war',
                 extinguishedLives: 0,
@@ -69,13 +75,16 @@ const disputeData = [
     },
     {
         id: 'inheritance',
+        title: 'The Large Inheritance',
         description: 'I am over 1300 years old and have ruled this planet for almost all my life. My time is coming to an end and I must pass the torch ...what would you advise?',
         leader: 'leader3.png',
+        leaderName: 'Grenuts',
+        bgColor: 'yellow',
         top: 12,
         left: 62,
         options: [
             {
-                optionId: 'first',
+                id: 'first',
                 text: 'you should make your first born the leader, I am sure they\'re a good kid',
                 result: 'after living a long and spoiled life your client\'s child is not equipped with the skills to lead, running the country into the ground. Society devolves into warring city states',
                 extinguishedLives: 100000,
@@ -84,7 +93,7 @@ const disputeData = [
 
             },
             {
-                optionId: 'whatever',
+                id: 'whatever',
                 text: 'you should\'t do anything, who cares you will be dead. They will figure it out',
                 result: 'your client dies and leaves no instruction as to who should take the throne. Thousands of insurgents fight their way to the capital to take the throne. This creates a 1000 year chain of usurping',
                 extinguishedLives: 8000000000,
@@ -92,7 +101,7 @@ const disputeData = [
                 anguish: 2         
             },
             {
-                optionId: 'vote',
+                id: 'vote',
                 text: 'You should tell the people you will be holding a planet wide vote, and have them elect a leader',
                 result: 'your client dies, but the people elect a fine leader from a small farming town with a background in agricultural sciences.',
                 extinguishedLives: 0,
--- dispute/dispute.js
+++ dispute/dispute.js
@@ -1,1 +1,56 @@
+import findById from '../utils.js';
+import disputes from '../dispute-data.js';
+import { getUser } from '../local-storage-utils.js';
 
+const user = getUser();
+
+//const body = document.querySelector('body');
+const disputeHeading = document.getElementById('dispute-heading');
+const disputeImgHeading = document.getElementById('dispute-img-heading');
+const disputeImg = document.getElementById('dispute-img');
+const disputeText = document.getElementById('dispute-text');
+const disputeChoices = document.getElementById('dispute-choices');
+
+const params = new URLSearchParams(window.location.search);
+
+const disputeId = params.get('id');
+
+const dispute = findById(disputeId, disputes);
+
+disputeHeading.textContent = dispute.title;
+disputeImgHeading.textContent = dispute.leaderName;
+disputeImg.src = `../assets/${dispute.leader}`;
+disputeText.textContent = dispute.description;
+
+for (const option of dispute.options) {
+    const choiceLabel = document.createElement('label');
+    const choiceInput = document.createElement('input');
+    choiceLabel.textContent = option.text;
+    
+    choiceInput.type = 'radio';
+    choiceInput.name = 'choice';
+    choiceInput.value = option.id;
+    choiceLabel.append(choiceInput);
+    disputeChoices.append(choiceLabel);
+}
+
+const chooseButton = document.createElement('button');
+chooseButton.textContent = 'choose';
+
+disputeChoices.append(chooseButton);
+
+disputeChoices.addEventListener('submit', (e) =>{
+    const formData = new FormData(disputeChoices);
+
+    e.preventDefault();
+
+    const optionId = formData.get('choice');
+    const option = findById(optionId, dispute.options);
+    user.anguish += option.anguish;
+    user.planetsSaved += option.planetsSaved;
+    user.extinguishedLives += option.extinguishedLives;
+    alert(`${option.result}... you extinguished ${option.extinguishedLives} lives`);
+
+    window.location = '../solar-system';
+    
+});
