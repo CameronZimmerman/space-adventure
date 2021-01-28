1f4b9aea96a31ae74efd37dab09b1f021e253b6b
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-28 09:57:57 -0800

    adjust tests

--- test/user-data.test.js
+++ test/user-data.test.js
@@ -1,27 +1,27 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { getLives, getEvil, getAnguish } from '../results/user-results-utils.js';
 
 const test = QUnit.test;
 
 const testUser = {
     evil: 0,
     anguish: 0,
-    livesExtinguished: 0,
+    extinguishedLives: 0,
 };
 // test user evil function
 test('based on users evil, return correct assesment key', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected1 = 'good';
-    const actual1 = getEvil(testUser.evil);
+    const actual1 = getEvil(testUser);
 
     testUser.evil = 5;
     const expected2 = 'gray';
-    const actual2 = getEvil(testUser.evil);
+    const actual2 = getEvil(testUser);
 
     testUser.evil = 10;
     const expected3 = 'evil';
-    const actual3 = getEvil(testUser.evil);
+    const actual3 = getEvil(testUser);
 
     testUser.evil = 0;
     //Act 
@@ -29,9 +29,9 @@ test('based on users evil, return correct assesment key', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual1, expected1);
-    expect.equal(actual2, expected2);
-    expect.equal(actual3, expected3);
+    expect.deepEqual(actual1, expected1);
+    expect.deepEqual(actual2, expected2);
+    expect.deepEqual(actual3, expected3);
 
 });
 
@@ -40,15 +40,15 @@ test('based on users anguish, return correct assesment key', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected1 = 'sane';
-    const actual1 = getAnguish(testUser.anguish);
+    const actual1 = getAnguish(testUser);
 
     testUser.anguish = 5;
     const expected2 = 'troubled';
-    const actual2 = getAnguish(testUser.anguish);
+    const actual2 = getAnguish(testUser);
 
     testUser.anguish = 10;
     const expected3 = 'insane';
-    const actual3 = getAnguish(testUser.anguish);
+    const actual3 = getAnguish(testUser);
 
     testUser.anguish = 0;
     //Act 
@@ -56,35 +56,35 @@ test('based on users anguish, return correct assesment key', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual1, expected1);
-    expect.equal(actual2, expected2);
-    expect.equal(actual3, expected3);
+    expect.deepEqual(actual1, expected1);
+    expect.deepEqual(actual2, expected2);
+    expect.deepEqual(actual3, expected3);
 
 });
 
 // test user lives function
-test('based on users anguish, return correct assesment key', (expect) => {
+test('based on users lives, return correct assesment key', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected1 = 'few';
-    const actual1 = getLives(testUser.livesExtinguished);
+    const actual1 = getLives(testUser);
 
-    testUser.livesExtinguished = 5;
+    testUser.extinguishedLives = 8000000000;
     const expected2 = 'many';
-    const actual2 = getLives(testUser.livesExtinguished);
+    const actual2 = getLives(testUser);
 
-    testUser.livesExtinguished = 10;
+    testUser.extinguishedLives = 24000000000;
     const expected3 = 'extreme';
-    const actual3 = getLives(testUser.livesExtinguished);
+    const actual3 = getLives(testUser);
 
-    testUser.livesExtinguished = 0;
+    testUser.extinguishedLives = 0;
     //Act 
     // Call the function you're testing and set the result to a const
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual1, expected1);
-    expect.equal(actual2, expected2);
-    expect.equal(actual3, expected3);
+    expect.deepEqual(actual1, expected1);
+    expect.deepEqual(actual2, expected2);
+    expect.deepEqual(actual3, expected3);
 
 });
