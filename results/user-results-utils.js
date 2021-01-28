115e3371304e0480807855763de9b14de6ef2f8e
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-28 09:49:12 -0800

    add functions to get user results

---
+++ results/user-results-utils.js
@@ -0,0 +1,24 @@
+
+export function getEvil(user) {
+    if (user.evil <= 2) return 'good';
+
+    if (user.evil > 2 && user.evil < 8) return 'gray';
+
+    if (user.evil >= 8) return 'evil';
+}
+
+export function getAnguish(user) {
+    if (user.anguish <= 2) return 'sane';
+
+    if (user.anguish > 2 && user.anguish < 8) return 'troubled';
+
+    if (user.anguish >= 8) return 'insane';
+}
+
+export function getLives(user) {
+    if (user.extinguishedLives <= 30000) return 'few';
+
+    if (user.extinguishedLives > 2 && user.extinguishedLives < 16000000000) return 'many';
+
+    if (user.extinguishedLives >= 16000000000) return 'extreme';
+}
